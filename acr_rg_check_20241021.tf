0d490375291f3e9f2c8b31e824386d2c9d44430e
Yibo Lu <lyb53708621@163.com>
2024-10-21 16:56:25 +0800

    rg check 20241021  -2

---
+++ acr_rg_check_20241021.tf
@@ -0,0 +1,14 @@
+data "azurerm_resource_group" "acr_terraform_rg" {
+  name = "acr_terraform_test"
+}
+
+output "display_rg_id" {
+  value = data.azurerm_resource_group.acr_terraform_rg.id
+}
+
+resource "azurerm_resource_group_policy_assignment" "acr_terraform_rg_check" {
+  name                 = "acr_terraform_rg_check"
+  display_name         = "acr_check_for_rg_acr_terraform"
+  resource_group_id    = data.azurerm_resource_group.acr_terraform_rg.id
+  policy_definition_id = azurerm_policy_definition.acr_premium_tier.id
+}
