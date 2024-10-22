4da191e87f7a795d1d542a07a0e228a3af2991e8
Yibo Lu <lyb53708621@163.com>
2024-10-22 14:17:53 +0800

    Revert 1

--- acr_rg_check_20241021.tf
+++ acr_rg_check_20241021.tf
@@ -1,14 +1,14 @@
-# data "azurerm_resource_group" "acr_terraform_rg" {
-#   name = "acr_terraform_test"
-# }
+data "azurerm_resource_group" "acr_terraform_rg" {
+  name = "acr_terraform_test"
+}
 
-# output "display_rg_id" {
-#   value = data.azurerm_resource_group.acr_terraform_rg.id
-# }
+output "display_rg_id" {
+  value = data.azurerm_resource_group.acr_terraform_rg.id
+}
 
-# resource "azurerm_resource_group_policy_assignment" "acr_terraform_rg_check_20241022" {
-#   name                 = "acr_terraform_rg_check_20241022"
-#   display_name         = "acr_terraform_rg_check_20241022"
-#   resource_group_id    = data.azurerm_resource_group.acr_terraform_rg.id
-#   policy_definition_id = azurerm_policy_definition.acr_premium_tier.id
-# }
+resource "azurerm_resource_group_policy_assignment" "acr_terraform_rg_check_20241022" {
+  name                 = "acr_terraform_rg_check_20241022"
+  display_name         = "acr_terraform_rg_check_20241022"
+  resource_group_id    = data.azurerm_resource_group.acr_terraform_rg.id
+  policy_definition_id = azurerm_policy_definition.acr_premium_tier.id
+}
